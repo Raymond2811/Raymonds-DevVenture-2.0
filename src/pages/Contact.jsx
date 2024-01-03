a223b412569e1d4480fef7c2d711a386e3bb1707
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-01-03 03:53:18 -0800

    added form and button

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,21 @@
+export default function Contact(){
+  return (
+    <div>
+      <h1>Contact</h1>
+      
+      <form>
+        <h2>Name:</h2>
+        <input/>
+        <h2>Email address:</h2>
+        <input/>
+        <h2>Message:</h2>
+        <input/>
+      </form>
+
+      <div>
+        <button/>
+      </div>
+
+    </div>
+  );
+};
