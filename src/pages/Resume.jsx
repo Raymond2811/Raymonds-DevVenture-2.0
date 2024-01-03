5292c029c22772146887bbf91d4eb10f0d08820a
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-01-03 02:48:00 -0800

    updated resume content

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,29 @@
+export default function Resume(){
+  return (
+    <div>
+      <h1>Resume</h1>
+
+      <h2> Front-end Proficiencies </h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>Responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+
+      <h2> Back-end Proficiencies</h2>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySql,Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </ul>
+    </div>
+  );
+}
