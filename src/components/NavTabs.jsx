1959f3909dc21cc1a0a23de20088f61ead7bd7f5
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-01-04 03:58:53 -0800

    added scripts and resolved errors

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -7,14 +7,6 @@ function NavTabs() {
     <ul className="nav justify-content-end">
       <li className="nav-item">
         <Link
-          to="/"
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
           to="/About"
           className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
         >
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App';
 import Error from './pages/Error';
-import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
@@ -20,10 +19,6 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
-      },
-      {
-        path: '/About',
         element: <About />,
       },
       {
