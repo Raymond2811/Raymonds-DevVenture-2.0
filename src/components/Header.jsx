211becf69f7e54d4ebbba1b7c912fb701949c4a1
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-01-04 04:35:22 -0800

    added name and NavTabs to Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavTabs from './NavTabs';
+
+export default function Header() {
+  return (
+    <header>
+      <h1> Raymond B </h1>
+      <NavTabs />
+    </header>
+  );
+}
