9282b0738b4bf236570dd46a30899b2bf5f9b148
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-01-10 21:36:28 -0800

    styled portfolio page and adjusted img size

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,18 +3,14 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Project({item}) {
   return(
-    <div>
+    <div className="container text-center projectContainer">
+      <img src={item.image} alt={item.title} />
       <a href={item.link} target="_blank">
-        <h2>{item.title}</h2>
+        <h3>{item.title}</h3>
       </a>
-      <a
-          href={item.repo}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <a href={item.repo} target="_blank" rel="noopener noreferrer" className="githubIcon">
           <FontAwesomeIcon icon={faGithub} size="3x"/>
-        </a>
-        <img src={item.image} alt={item.title} />
+      </a>
     </div>
   )
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,10 @@
 import Project from "../components/Project";
 import ReelMeal from "../assets/images/Reel&Meal.png";
-import EzBank from "../assets/images/EzBank.png";
-import JATE from "../assets/images/JATE.png";
-import portfolioV1 from "../assets/images/Portfolio.png";
-import SkyInsight from "../assets/images/SkyInsight.png";
-import soon from "../assets/images/coming-soon.jpeg";
+// import EzBank from "../assets/images/EzBank.png";
+// import JATE from "../assets/images/JATE.png";
+// import portfolioV1 from "../assets/images/Portfolio.png";
+// import SkyInsight from "../assets/images/SkyInsight.png";
+// import soon from "../assets/images/coming-soon.jpeg";
 
 
 export default function Portfolio(){
@@ -21,12 +21,13 @@ export default function Portfolio(){
       title: "EzBank",
       link:'https://ezbank-6a5a197718d2.herokuapp.com/homepage',
       repo: 'https://github.com/Raymond2811/EzBank',
-      image: EzBank,
+      // image: EzBank,
     },
     {
       id: 3,
       title: "Coming Soon!",
-      image: soon,
+      
+      // image: soon,
 
     },
     {
@@ -34,29 +35,33 @@ export default function Portfolio(){
       title: "SkyInsight",
       link:'https://raymond2811.github.io/SkyInsight/',
       repo: 'https://github.com/Raymond2811/SkyInsight',
-      image: SkyInsight,
+      // image: SkyInsight,
     },
     {
       id: 5,
       title: "JATE",
       link:'https://editepic.onrender.com/',
       repo: 'https://github.com/Raymond2811/EditEpic',
-      image: JATE,
+      // image: JATE,
     },
     {
       id: 6,
       title: "Raymond's DevVenture",
       link:'https://raymond2811.github.io/Raymonds-DevVenture/',
       repo: 'https://github.com/Raymond2811/Raymonds-DevVenture',
-      image: portfolioV1,
+      // image: portfolioV1,
     }
   ]
   return (
-    <div>
+    <div className="portfolioContainer">
       <h1>Portfolio</h1>
-      {projects.map(item=> (
-        <Project item={item} key={item.id}/>
-      ))}
+      <div className="row">
+        {projects.map((item, index) => (
+          <div key={item.id} className="col-md-6 projectItem">
+            <Project item={item} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
