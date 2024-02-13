61e0f6a22f45c8bb5a8b4833aef4b6a86c4b502e
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-02-13 02:19:19 -0800

    updated about me content and changed positioning

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,51 +4,61 @@ export default function About(){
   return (
     <section className="aboutContainer">
       <h1>About Me</h1>
-      <img src={avatar} alt={avatar}/>
-      <p>
-        Hi, I'm Raymond Bautista, 21 years old, and I have lived in San Bruno, CA all my life. 
-        My family is Peruvian, making me a first-generation Peruvian American. 
-        When I was around 14 or 15, I started working at a bakery to help my mom. 
-        Part of me wished I hadn't taken the job because it was tiresome and extremely cold, 
-        working in the freezer making sandwiches alongside my grandpa, starting at 7 am. 
-        Despite the challenges, I'm glad I could help my mom and meet some cool people.
-      </p>
-      <p>
-        As soon as I turned 16, I began searching for a job and landed one at JCPenney. 
-        I enjoyed working there because most of the workers were around my age, and we got along well, 
-        making the time pass quickly. 
-        After a year, I decided to seek a new job for higher pay and ended up at Best Buy. 
-      </p>
-      <p>
-        Noticing that Best Buy couldn't offer more hours, 
-        I continued working at JCPenney until I finished paying for my Invisalign. 
-        Eventually, COVID hit, and I quit JCPenney; by the end of the year, the Best Buy store closed down. 
-        Instead of looking for another job, I decided to focus on my classes as they became more challenging 
-        at Skyline Community College, where I initially pursued electrical engineering.
-      </p>
-      <p>
-        A year later, I realized electrical engineering wasn't for me. 
-        One day, at my uncle's house, I overheard a job opportunity, decided to take it without hesitation, 
-        and landed a job as an assembler at Cutera. 
-        Despite having no experience in assembling, my boss assured me they would teach me everything I needed to know. 
-        The job was relatively simple, involving assembling hand pieces for a bigger machine.
-      </p>
-      <p> 
-        I eventually quit when the fall semester started. 
-        During fall, I discovered a free two-week front-end course, tried it, liked it, 
-        and decided to take coding classes at my school. 
-        Though I didn't enjoy the classes offered, I still wanted to learn.
-      </p>
-      <p>
-        A friend recommended a bootcamp, and although I initially didn't plan to apply, 
-        I clicked on the survey to see if it was right for me. 
-        I ended up scheduling an interview with the school, thinking it was a questionnaire. 
-        I passed the interview and got in. 
-        So far, I find the lectures interesting, and it's cool to learn how apps are built. 
-        In my free time, I watch shows/movies, play video games, and hang out with friends. 
-        Some of my favorite shows include Snowfall, Better Call Saul, and One Piece. 
-        Currently, my favorite game is Rainbow 6 Siege.
-      </p>
+      <div className="row">
+        <div className="imageContainer">
+          <img src="image1.jpg" alt="Image 1" />
+        </div>
+        <div className="textContainer">
+          <p>
+            Hello! I'm Raymond Bautista, a recent graduate and aspiring full-stack developer. 
+            I'm excited to bring my fresh perspective and knowledge to real-world projects.
+          </p>
+          <p>
+            Proficient in a range of front-end technologies such as React, JQuery, and more, 
+            I also possess back-end proficiencies, including APIs, Node.js, MongoDB, and other 
+            technologies. My academic projects have equipped me with a solid foundation for 
+            crafting responsive and user-friendly web applications.
+          </p>
+          <p>
+            I thrive on tackling challenges and turning them into opportunities for growth. 
+            My ability to solve problems and pay close attention to details allows me to 
+            contribute effectively in team-based development settings.
+          </p>
+          <p>
+            In my spare time, I enjoy watching shows and movies, playing video games, and 
+            spending time with friends. Some of my favorites include Snowfall, Better Call Saul, 
+            and One Piece. Currently, Rainbow 6 Siege is my go-to game.
+          </p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="textContainer">
+          <p>
+            I graduated from UC Berkeley Extension, where I honed my 
+            technical skills and developed a passion for applying technology to real-world 
+            challenges.
+          </p>
+          <p>
+            As a recent graduate, I'm enthusiastic about continuous learning. 
+            I am committed to staying updated on the latest industry trends and technologies, 
+            ensuring that my skills are always aligned with the dynamic demands of the field.
+          </p>
+          <p>
+            Throughout my academic journey, I actively participated in group projects, 
+            fostering strong communication and collaboration skills. I'm ready to 
+            bring this collaborative spirit to a professional setting.
+          </p>
+          <p>
+            I'm on the lookout for opportunities to apply and expand my skills. 
+            If you're seeking a dedicated and quick-learning full-stack developer to 
+            contribute to your team, let's connect.
+          </p>
+        </div>
+        <div className="imageContainer">
+          <img src="image2.jpg" alt="Image 2" />
+        </div>
+      </div>
     </section>
   );
 }
