83bbb9a5fc0dfd7c7470a75e23f463c744b37054
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-02-20 23:43:05 -0800

    positioned elements

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,30 +4,36 @@ export default function Resume(){
   return (
     <section className='resumeContainer'>
       <h1>Resume</h1>
-      
-      <p>Download my <a href={resume} target='_blank'>resume</a></p>
+      <div className='row'>
+        <div className='resumeColumn'>
+          <p>Download my <a href={resume} target='_blank'>resume</a></p>
 
-      <h2> Front-end Proficiencies </h2>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>JQuery</li>
-        <li>Responsive design</li>
-        <li>React</li>
-        <li>Bootstrap</li>
-      </ul>
+          <h2> Front-end Proficiencies </h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li>Responsive design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
 
-      <h2> Back-end Proficiencies</h2>
-      <ul>
-        <li>APIs</li>
-        <li>Node</li>
-        <li>Express</li>
-        <li>MySql, Sequelize</li>
-        <li>MongoDB, Mongoose</li>
-        <li>REST</li>
-        <li>GraphQL</li>
-      </ul>
+          <h2> Back-end Proficiencies</h2>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySql, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+        <div className='imageColumn'>
+          <img src={resume} alt='resume'/>
+        </div>
+      </div>
     </section>
   );
 }
