2972e5ebe1d4d88190c8b7568ea76f7ac0e8bc17
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-03-17 18:33:03 -0700

    added drawer for mobile users

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,5 +1,7 @@
-import { Link, useLocation } from 'react-router-dom';
-import { Tabs, Tab } from '@mui/material';
+import { Link, useLocation} from 'react-router-dom';
+import { Tabs, Tab, Drawer, IconButton  } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 import './NavTabs.css'
 
 function NavTabs() {
@@ -15,23 +17,74 @@ function NavTabs() {
 
   const currentIndex = tabData.findIndex((tab) => tab.to === currentPage);
 
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  }
+
   return (
-    <Tabs 
-      className='navTab' 
-      value={currentIndex !== -1 ? currentIndex : false}
-    >
-      {tabData.map((tab, index) => (
-        <Tab
-          style={{color:'white'}}
-          key={index}
-          label={tab.label}
-          component={Link}
-          to={tab.to}
-          value={index}
-          className={`${currentIndex === index ? 'activeTab' : 'defaultTab'}`}
-        />
-      ))}
-    </Tabs>
+    <nav className='navContainer'>
+      <div className='navbar'>
+        <Tabs 
+          className='navTab' 
+          value={currentIndex !== -1 ? currentIndex : false}
+        >
+          {tabData.map((tab, index) => (
+            <Tab
+              style={{color:'white'}}
+              key={index}
+              label={tab.label}
+              component={Link}
+              to={tab.to}
+              value={index}
+              className={`${currentIndex === index ? 'activeTab' : ''}`}
+            />
+          ))}
+        </Tabs>
+      </div>
+
+      {/* drawer for smaller screens */}
+
+      <div className='sidebar'>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge='end'
+          onClick={handleDrawerToggle}
+        >
+          <MenuIcon fontSize='large'/>
+        </IconButton>
+        <Drawer
+          anchor='right'
+          open={drawerOpen}
+          onClose={handleDrawerToggle}
+        >
+          <div
+            role="presentation"
+            onClick={handleDrawerToggle}
+            onKeyDown={handleDrawerToggle}
+          >
+            <Tabs
+              value={currentIndex !== -1 ? currentIndex : false}
+              orientation='vertical'
+              variant='scrollable'
+            >
+              {tabData.map((tab,index) => (
+                <Tab
+                  key={index}
+                  label={tab.label}
+                  component={Link}
+                  to={tab.to}
+                  value={index}
+                  className={`${currentIndex === index ? 'activeTab' : ''}`}
+                />
+              ))}
+            </Tabs>
+          </div>
+        </Drawer>
+      </div>
+    </nav>
   )
 }
 
